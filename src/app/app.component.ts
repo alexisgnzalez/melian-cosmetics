358203ad2bc57a4aa0833c3f81c9be379130478c
Alexis Gonzalez <1991.alexis@gmail.com>
2024-06-01 11:35:51 -0400

    created login page and added service to login with a magic link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './pages/login/login.page'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseKey: 'YOUR_SUPABASE_KEY',
+  supabaseUrl: 'https://ulzylfrziikkdhonsdui.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsenlsZnJ6aWlra2Rob25zZHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY5NDU5MzMsImV4cCI6MjAzMjUyMTkzM30.VPxnpGSZXONWhe4ZsVL1uG8PKWDbgoztus420yYobLY',
 }
