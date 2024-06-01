7e58648571b2e14c28ad7ab7149e43337a91ec08
Alexis Gonzalez <1991.alexis@gmail.com>
2024-06-01 12:06:59 -0400

    added account page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginPage } from './pages/login/login.page'
+import { SupabaseService } from './services/supabase/supabase.service';
+import { AccountPage } from './pages/account/account.page';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginPage],
+  imports: [RouterOutlet, LoginPage, AccountPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'melian-cosmetics';
+  session = this.supabase.session;
+
+  constructor(private readonly supabase: SupabaseService) {}
+
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session))
+  }
 }
