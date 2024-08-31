e3b3673560ba07eb42eb0a505f22287964d6bb6d
Alexis Gonzalez <1991.alexis@gmail.com>
2024-08-31 15:50:25 -0400

    restored the whole project

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # MelianCosmetics
 
+Página web para venta de cosméticos de alta calidad
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.0.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from './pages/login/login.page'
+import { SupabaseService } from './services/supabase/supabase.service';
+import { AccountPage } from './pages/account/account.page';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LoginPage, AccountPage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'melian-cosmetics';
+  session = this.supabase.session;
+
+  constructor(private readonly supabase: SupabaseService) {}
+
+  ngOnInit() {
+    this.supabase.authChanges((_, session) => (this.session = session))
+  }
 }
