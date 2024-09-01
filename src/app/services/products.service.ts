10e7a1d182cb52df905649c286a4097cf15495aa
Alexis Gonzalez <1991.alexis@gmail.com>
2024-09-01 19:59:19 -0400

    erased console logs

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -13,7 +13,6 @@ export class ProductsService {
     const pb = new PocketBase(environment.baseUrl);
     // you can also fetch all records at once via getFullList
     const resultList = await pb.collection('products').getFullList();
-    console.log(resultList);
     return resultList;
   }
 }
