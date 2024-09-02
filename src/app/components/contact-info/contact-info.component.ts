eb7fa649121d5afd8e298cff30f4c382843daad2
Alexis Gonzalez <1991.alexis@gmail.com>
2024-09-01 22:47:15 -0400

    added contact page correctly

---
+++ src/app/components/contact-info/contact-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-contact-info',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './contact-info.component.html',
+  styleUrl: './contact-info.component.scss'
+})
+export class ContactInfoComponent {
+
+}
